d015654495c0e480e22d96011022da9df9c7cfa3
Enry99 <enri.p25@gmail.com>
2025-06-18 01:09:52 +0200

    Added description docstring to init

--- xplot/__init__.py
+++ xplot/__init__.py
@@ -1,3 +1,28 @@
-__version__ = '1.0'
+# Created on Tue 28 Nov 2023
 
-POVRAY_OLD_STYLE = False
+"""
+--------------------------------- atomsplot ------------------------------------
+
+Small program to plot images of atomic structures.
+
+@author: Enrico Pedretti
+
+Key features:
+- Read atomic structures from various formats (ASE, VASP, etc.)
+- Generate high-quality images using PovRay
+- Colorcode atoms by forces, coordination numbers, magnetic moments
+- Draw arrows to visualize forces and magnetic moments
+- Depth cueing to better see the depth using fog
+- Generate movies from trajectory files
+- Plot charge density isosurfaces
+
+------------------------------ Useful links ------------------------------------
+Repository: https://github.com/Enry99/atomsplot
+
+Realized as a spin-off of Xsorb, a tool to automate molecular adsorption:
+E. Pedretti, P. Restuccia, M.C. Righi, Comput. Phys. Commun. 291 (2023), 108827
+https://doi.org/10.1016/j.cpc.2023.108827 (https://github.com/Enry99/xsorb)
+
+"""
+
+__version__ = '1.0'
